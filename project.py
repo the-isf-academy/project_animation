53857eb405f95551b54a1770574c8616a3637b7f
emma <eqbrown@isf.edu.hk>
2024-10-02 08:08:14 +0800

    DAY ONE #today init settings, minor edits to project #next test commit template

--- project.py
+++ project.py
@@ -1,10 +1,17 @@
 from turtle import *
 from superturtle.animation import *
 from superturtle.movement import *
+import settings
 
+colormode(255)      # rgb color mode
+screen = Screen()
+screen.setup(settings.WIDTH, settings.HEIGHT)   # sets custom screen size
 
-for frame in animate(frames=60):
-    pass  # delete this and write your own code!   
+bgcolor(settings.BACKGROUND_COLOR)  #sets background color 
+
+for frame in animate(frames=settings.TOTAL_FRAMES):
+    pass  # delete this line and write your own code!  
+
 
 
 
