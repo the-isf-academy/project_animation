9b66746f5c96035156fcfa609218ae96e5356b57
emma <eqbrown@isf.edu.hk>
2024-10-16 08:49:36 +0800

    minor reformating of comments

--- project.py
+++ project.py
@@ -3,11 +3,11 @@ from superturtle.animation import *
 from superturtle.movement import *
 import settings
 
-colormode(255)      # rgb color mode
+colormode(255)                                  # rgb color mode
 screen = Screen()
 screen.setup(settings.WIDTH, settings.HEIGHT)   # sets custom screen size
 
-bgcolor(settings.BACKGROUND_COLOR)  #sets background color 
+bgcolor(settings.BACKGROUND_COLOR)              #sets background color 
 
 for frame in animate(frames=settings.TOTAL_FRAMES):
     pass  # delete this line and write your own code!  
